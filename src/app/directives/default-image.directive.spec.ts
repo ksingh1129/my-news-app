dbfa5a9f0b32054a20ce8be5344eb5bcdf28571b
Khomkrib Singh <khomkrib.s@gmail.com>
2019-12-20 21:58:42 +0700

    add unit test default image directive

--- src/app/directives/default-image.directive.spec.ts
+++ src/app/directives/default-image.directive.spec.ts
@@ -5,4 +5,30 @@ describe('DefaultImageDirective', () => {
     const directive = new DefaultImageDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should return default image url when the url is null', () => {
+    const directive = new DefaultImageDirective();
+
+    const expectedUrl = 'http://lamp.oja.go.th/medianewsIMG/default-image.png';
+
+    expect(directive.checkPath(null)).toEqual(expectedUrl);
+  });
+
+  it('should return image url', () => {
+    const directive = new DefaultImageDirective();
+    const imageUrl = 'https://media.geeksforgeeks.org/wp-content/uploads/20190719161411/cat1-225x300.jpg';
+    expect(directive.checkPath(imageUrl)).toEqual(imageUrl);
+  })
+
+  it('should return default image url when error', () => {
+    const directive = new DefaultImageDirective();
+    const errorImageUrl = 'xxx';
+    const expectedUrl = 'http://lamp.oja.go.th/medianewsIMG/default-image.png';
+
+    directive.src = errorImageUrl;
+    directive.onError();
+
+    expect(directive.src).toEqual(expectedUrl)
+  });
+
 });
