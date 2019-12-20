fe7848aa905b102a2e57f4c8541b83bfb2e3bea6
Khomkrib Singh <khomkrib.s@gmail.com>
2019-12-20 13:31:34 +0700

    add firebase host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyCfosQWRM6qKiWQbtmOYDzHtIHOWxpJzC8",
+  authDomain: "my-news-app-5c930.firebaseapp.com",
+  databaseURL: "https://my-news-app-5c930.firebaseio.com",
+  projectId: "my-news-app-5c930",
+  storageBucket: "my-news-app-5c930.appspot.com",
+  messagingSenderId: "304419266484",
+  appId: "1:304419266484:web:6129a1c357fc8109eadce1"
 };
